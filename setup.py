e4a995f0133c590bf85b36f7995cb4e638fd79b9
Valentin <valentin.bouis@gmail.com>
2023-01-21 15:58:54 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+  
+setup(
+    name='optus-server',
+    version='0.1',
+    description='Optus server',
+    install_requires=[
+        'Flask',
+        'Flask-Cors',
+    ],
+)
